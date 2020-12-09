c94de64fa67031ee54d2cf6b59f8b92fd8197f5e
Samat Tulegenov <magnit56@gmail.com>
2020-12-09 07:23:20 +0300

    created help app

---
+++ src/gendiff.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GenDiff\GenDiff;
+
+function printHelp()
+{
+    $doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+DOC;
+
+    $params = array(
+        'argv'=>array_slice($_SERVER['argv'], 1),
+        'help'=>true,
+        'version'=>null,
+        'optionsFirst'=>false,
+    );
+    $args = \Docopt::handle($doc, $params);
+    foreach ($args as $k=>$v)
+        print_r($k.': '.json_encode($v).PHP_EOL);
+}
