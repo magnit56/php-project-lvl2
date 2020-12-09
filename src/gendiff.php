cf12f0e612e1a6f944b5bcbb6c7082e45d0a0af8
Samat Tulegenov <magnit56@gmail.com>
2020-12-09 07:50:08 +0300

    solved lint problems

--- src/gendiff.php
+++ src/gendiff.php
@@ -17,12 +17,12 @@ Options:
 DOC;
 
     $params = array(
-        'argv'=>array_slice($_SERVER['argv'], 1),
-        'help'=>true,
-        'version'=>null,
-        'optionsFirst'=>false,
+        'argv' => array_slice($_SERVER['argv'], 1),
+        'help' => true,
+        'version' => null,
+        'optionsFirst' => false,
     );
     $args = \Docopt::handle($doc, $params);
-    foreach ($args as $k=>$v)
+    foreach ($args as $k => $v)
         print_r($k.': '.json_encode($v).PHP_EOL);
 }
