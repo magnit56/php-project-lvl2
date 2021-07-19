5844038a604f86bd026e9d7a71b8a9d3840f70f7
Samat Tulegenov <magnit56@gmail.com>
2021-07-19 15:43:49 +0500

    Добавлены типы в функцию render, switch заменен на match

--- src/Renderers.php
+++ src/Renderers.php
@@ -2,18 +2,21 @@
 
 namespace Differ\Renderers;
 
+use Exception;
+
 use function Differ\Formatters\renderStylish;
 use function Differ\Formatters\renderPlain;
 use function Differ\Formatters\renderJson;
 
-function render($ast, $format)
+/**
+ * @throws Exception
+ */
+function render(array $ast, string $format): string
 {
-    switch ($format) {
-        case 'stylish':
-            return renderStylish($ast);
-        case 'plain':
-            return renderPlain($ast);
-        case 'json':
-            return renderJson($ast);
-    }
+    return match ($format) {
+        'stylish' => renderStylish($ast),
+        'plain' => renderPlain($ast),
+        'json' => renderJson($ast),
+        default => throw new Exception('Такой формат не поддерживается программой'),
+    };
 }
