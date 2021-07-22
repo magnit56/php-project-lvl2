8717b46647aef059b2d01970139e85494ed468b6
Samat Tulegenov <magnit56@gmail.com>
2021-07-22 13:24:41 +0500

    Удалена лишняя строка

--- src/Runner.php
+++ src/Runner.php
@@ -9,9 +9,8 @@ use function Differ\Differ\genDiff;
 
 function run(string $doc): void
 {
-    $args = Docopt::handle($doc, ['version' => 'Version 1.0']);
-
     try {
+        $args = Docopt::handle($doc, ['version' => 'Version 1.0']);
         print_r(genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']));
     } catch (Exception $exception) {
         print_r($exception->getMessage());
