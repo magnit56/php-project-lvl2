45ec36011c6ec0d1c8b66b559b38323b59428ffc
Samat Tulegenov <magnit56@gmail.com>
2021-07-22 13:34:48 +0500

    Изменен вывод результата

--- src/Runner.php
+++ src/Runner.php
@@ -11,8 +11,9 @@ function run(string $doc): void
 {
     try {
         $args = Docopt::handle($doc, ['version' => 'Version 1.0']);
-        print_r(genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']));
+        $output = genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
     } catch (Exception $exception) {
-        print_r($exception->getMessage());
+        $output = $exception->getMessage();
     }
+    print_r($output);
 }
